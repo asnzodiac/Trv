5b50140480a9d00a1b7eed29345b2024291c1a74
Aswin M <50163512+asnzodiac@users.noreply.github.com>
2026-03-08 03:38:57 +0530

    Add service worker install and fetch event listeners

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installed");
+});
 
+self.addEventListener("fetch", (e) => {
+  // Optional: Cache strategies can be added here
+});
